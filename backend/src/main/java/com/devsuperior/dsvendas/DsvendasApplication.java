976762aabaa9f362157d4ba3096d8870c69f807b
Bruno de Almeida <brunodealmeida17@yahoo.com>
2021-09-08 16:45:37 -0300

    Security config

--- backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
+++ backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
@@ -1,4 +1,4 @@
-package com.insonictech.dsvendas;
+package com.devsuperior.dsvendas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
